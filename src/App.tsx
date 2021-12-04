3a25abf4d0d74e85b76664a366e6a66a298f075c
Murendeni Mukwevho <msiphonic@gmail.com>
2021-12-04 09:29:56 +0200

    moved audio player to app component

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,30 @@ import Home from './pages/Home';
 import Song from './pages/Song';
 import Songs from './pages/Songs';
 import UploadSongs from './pages/UploadSongs';
+import AudioPlayer from './components/AudioPlayer';
+import { useSelector } from 'react-redux';
+import { State } from './state';
 
 const App = () => {
+    const state = useSelector((state: State) => state.audioPlayer)
+
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact children={<Home />} />
-                <Route path="/songs" exact children={<Songs />} />
-                <Route path="/song/:id" exact children={<Song />} />
-                <Route path="/artists" exact children={<Artists />} />
-                <Route path="/artist/:id" exact children={<Artist />} />
-                <Route path="/upload" exact children={<UploadSongs />} />
-                <Redirect to="/" />
-            </Switch>
-        </Router>
+        <>
+            <Router>
+                <Switch>
+                    <Route path="/" exact children={<Home />} />
+                    <Route path="/songs" exact children={<Songs />} />
+                    <Route path="/song/:id" exact children={<Song />} />
+                    <Route path="/artists" exact children={<Artists />} />
+                    <Route path="/artist/:id" exact children={<Artist />} />
+                    <Route path="/upload" exact children={<UploadSongs />} />
+                    <Redirect to="/" />
+                </Switch>
+            </Router>
+            {state.playerActive && (
+                <AudioPlayer />
+            )}
+        </>
     );
 }
 
